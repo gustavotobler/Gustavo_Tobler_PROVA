70ca875af7ace22a918a05f0d869f21a54cebc04
gustavotobler <gustavotobler70@gmail.com>
2025-08-21 16:58:41 -0300

    ...
    ...

--- menu_nav.php
+++ menu_nav.php
@@ -87,7 +87,7 @@ $permissoes = [
 $opcoes_menu = $permissoes[$id_perfil];
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
